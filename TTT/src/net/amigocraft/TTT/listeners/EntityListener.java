4709e5f3747c84607f7d87301d7f629e1c8edb9a
Maxim Roncace <mproncace@gmail.com>
2013-05-27 21:34:51 -0400

    Implemented Listener in EntityListener and corrected version number

--- TTT/src/net/amigocraft/TTT/listeners/EntityListener.java
+++ TTT/src/net/amigocraft/TTT/listeners/EntityListener.java
@@ -2,13 +2,14 @@ package net.amigocraft.TTT.listeners;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 
 import net.amigocraft.TTT.Round;
 import net.amigocraft.TTT.TTTPlayer;
 
-public class EntityListener {
+public class EntityListener implements Listener {
 
 	@EventHandler
 	public void onEntityTarget(EntityTargetEvent e){
