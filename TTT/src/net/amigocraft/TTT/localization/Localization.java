93768271423b9e32f2b0fa85704bc50ec95c12c3
Maxim Roncace <mproncace@gmail.com>
2013-05-30 22:17:46 -0400

    Added easily-configurable locales

--- TTT/src/net/amigocraft/TTT/localization/Localization.java
+++ TTT/src/net/amigocraft/TTT/localization/Localization.java
@@ -1,5 +1,7 @@
 package net.amigocraft.TTT.localization;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -11,16 +13,26 @@ import org.bukkit.ChatColor;
 
 public class Localization {
 
-	public TTT plugin = TTT.plugin;
-
 	public String getMessage(String key){
 		InputStream is = null;
 		is = Localization.class.getResourceAsStream("/net/amigocraft/TTT/localization/" + TTT.lang + ".properties");
-		if (is == null)
-			is = Localization.class.getResourceAsStream("/net/amigocraft/TTT/localization/enUS.properties");
+		if (is == null){
+			try {
+				File file = new File(TTT.plugin.getDataFolder() + File.separator + "locales" + File.separator + TTT.lang + ".properties");
+				is = new FileInputStream(file);
+				if (TTT.plugin.getConfig().getBoolean("verbose-logging"))
+					TTT.plugin.log.info("Loaded locale from " + file.getAbsolutePath());
+			}
+			catch (Exception ex){
+				is = Localization.class.getResourceAsStream("/net/amigocraft/TTT/localization/enUS.properties");
+				if (TTT.plugin.getConfig().getBoolean("verbose-logging"))
+					TTT.plugin.log.info("Locale defined in config not found in JAR or plugin folder; defaulting to enUS");
+			}
+		}
 		StringWriter writer = new StringWriter();
 		try {
 			IOUtils.copy(is, writer, "ISO-8859-1");
+			is.close();
 			String contents = writer.toString();
 			String[] lines = contents.split("\n");
 			for (int i = 0; i < lines.length; i++){
