37b8ce9443ba919eafbbce08798d425b9f384ded
mproncace <mproncace@gmail.com>
2015-01-01 19:55:26 -0500

    Switch version to 0.7.0-SNAPSHOT and update copyright

--- src/main/java/net/amigocraft/ttt/managers/command/admin/KickCommand.java
+++ src/main/java/net/amigocraft/ttt/managers/command/admin/KickCommand.java
@@ -1,6 +1,6 @@
 /*
  * TTT
- * Copyright (c) 2014, Maxim Roncacé <http://bitbucket.org/mproncace>
+ * Copyright (c) 2013-2015, Maxim Roncacé <mproncace@lapis.blue>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
