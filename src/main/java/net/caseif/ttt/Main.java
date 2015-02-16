78badc9803f66fb5af80c544deed6ce2866111a8
caseif <mproncace@gmail.com>
2015-02-15 20:19:56 -0500

    Remove star import

--- src/main/java/net/caseif/ttt/Main.java
+++ src/main/java/net/caseif/ttt/Main.java
@@ -39,7 +39,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import net.amigocraft.mglib.MGUtil;
