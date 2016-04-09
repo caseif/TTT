3b81bec8695d5fd0f66f788d2f8db786038be02b
Max Roncace <me@caseif.net>
2016-04-09 19:44:25 -0400

    Add BungeeCord as dependency; fix relocation for jTelemetry

---
+++ src/main/java/net/caseif/ttt/util/helper/platform/BungeeHelper.java
@@ -0,0 +1,11 @@
+package net.caseif.ttt.util.helper.platform;
+
+import org.bukkit.entity.Player;
+
+public class BungeeHelper {
+
+    public static void sendPlayerToReturnServer(Player player) {
+
+    }
+
+}
